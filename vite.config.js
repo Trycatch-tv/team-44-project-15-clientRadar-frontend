dcf7ba4fa28620b14cab6fe39c0f98ff650d31ca
imCsCorp <camilo.alejandro.soto.v@gmail.com>
2023-04-22 00:49:14 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://trycatch-tv.github.io/team-44-project-15-clientRadar-frontend/"
+  base: "https://trycatch-tv.github.io/team-44-project-15-clientRadar-frontend/"
 })
